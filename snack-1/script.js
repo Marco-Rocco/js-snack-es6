85d92aa6ddb9c5e14e10c969a4fbcbf5653336ef
Marco-Rocco <marcorocco28@icloud.com>
2025-03-07 16:42:46 +0100

    feat: create array and objects

---
+++ snack-1/script.js
@@ -0,0 +1,43 @@
+let bici1 = {
+    "colore": "rosso",
+    "peso-kg": 10,
+    "prezzo": 500,
+}
+
+let bici2 = {
+    "colore": "nero",
+    "peso-kg": 15,
+    "prezzo": 300,
+}
+
+let bici3 = {
+    "colore": "giallo",
+    "peso-kg": 8,
+    "prezzo": 750,
+}
+
+let bici4 = {
+    "colore": "rosa",
+    "peso-kg": 9,
+    "prezzo": 600,
+}
+
+let bici5 = {
+    "colore": "azzurro",
+    "peso-kg": 12,
+    "prezzo": 400,
+}
+
+
+
+
+let biciclette = [
+    bici1,
+    bici2,
+    bici3,
+    bici4,
+    bici5,
+]
+
+
+console.log(biciclette);
